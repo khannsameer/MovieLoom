75eb7b98a587b6a36b3c66ffd917194b5d415031
khannsameer <sk8717108@gmail.com>
2025-12-30 20:22:56 +0530

    feat: update in search page

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -12,7 +12,7 @@ const SearchPage = () => {
 
   const fetchData = async () => {
     try {
-      const res = await axios.get(`/search/collection`, {
+      const res = await axios.get(`/search/multi`, {
         params: {
           query: location?.search?.slice(3),
           page: page,
@@ -63,8 +63,8 @@ const SearchPage = () => {
           />
         </div>
       </div>
-      <div className="container mx-auto px-3 sm:px-4">
-        <h3 className="capitalize text-lg lg:text-xl font-semibold my-3">
+      <div className="container mx-auto">
+        <h3 className="capitalize text-lg lg:text-xl font-semibold my-3 px-3">
           Search Results
         </h3>
         <div className="grid grid-cols-[repeat(auto-fit,230px)] gap-7 justify-center lg:justify-start">
